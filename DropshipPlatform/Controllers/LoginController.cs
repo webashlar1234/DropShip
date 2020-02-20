96274084090a384fb03d1ccee6adb44bd1e7bc60
sankar_wa <ssankarwa@gmail.com>
2020-02-20 18:59:18 +0530

    Fixed UI issues

--- DropshipPlatform/Controllers/LoginController.cs
+++ DropshipPlatform/Controllers/LoginController.cs
@@ -32,7 +32,7 @@ namespace DropshipPlatform.Controllers
                     UserModel user = (UserModel)response.Data;
                     Session["UserName"] = user.Name;
                     Session["UserID"] = user.UserID;
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "AliExpress");
                 }
             }
             catch (Exception ex)
