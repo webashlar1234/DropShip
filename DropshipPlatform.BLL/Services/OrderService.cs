0971b7f7a3b8c355b6d6d6fc658b68956d5848e9
sankar-wa <ssankarwa@gmail.com>
2020-03-06 18:44:56 +0530

    Order process ship now changes

--- DropshipPlatform.BLL/Services/OrderService.cs
+++ DropshipPlatform.BLL/Services/OrderService.cs
@@ -25,13 +25,14 @@ namespace DropshipPlatform.BLL.Services
                 AliexpressSolutionOrderGetRequest req = new AliexpressSolutionOrderGetRequest();
 
                 AliexpressSolutionOrderGetRequest.OrderQueryDomain obj1 = new AliexpressSolutionOrderGetRequest.OrderQueryDomain();
-                obj1.CreateDateEnd = "2020-03-05 12:12:12";
-                obj1.CreateDateStart = "2020-03-01 12:12:12";
-                obj1.ModifiedDateStart = "2020-03-01 12:12:12";
+                var todayDate = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                obj1.CreateDateEnd = todayDate;
+                obj1.CreateDateStart = "2020-03-01 00:00:00";
+                obj1.ModifiedDateStart = "2020-03-01 00:00:00";
                 obj1.OrderStatusList = new List<string> { "SELLER_PART_SEND_GOODS", "PLACE_ORDER_SUCCESS", "IN_CANCEL", "WAIT_SELLER_SEND_GOODS", "WAIT_BUYER_ACCEPT_GOODS", "FUND_PROCESSING" , "IN_ISSUE", "IN_FROZEN", "WAIT_SELLER_EXAMINE_MONEY", "RISK_CONTROL", "FINISH" };
                 obj1.BuyerLoginId = "edacan0107@aol.com";
                 obj1.PageSize = 20L;
-                obj1.ModifiedDateEnd = "2020-03-05 12:12:12"; 
+                obj1.ModifiedDateEnd = todayDate; 
                 obj1.CurrentPage = 1L;
                 obj1.OrderStatus = "SELLER_PART_SEND_GOODS";
                 req.Param0_ = obj1;
@@ -63,6 +64,53 @@ namespace DropshipPlatform.BLL.Services
                 logger.Error(ex.ToString());
             }
             return products;
+        }
+
+        public bool trackingOrder(OrderData orderData)
+        {
+            bool result = true;
+
+            try
+            {
+                ITopClient client = new DefaultTopClient(StaticValues.aliURL, StaticValues.aliAppkey, StaticValues.aliSecret);
+
+                AliexpressLogisticsRedefiningGetonlinelogisticsservicelistbyorderidRequest req1 = new AliexpressLogisticsRedefiningGetonlinelogisticsservicelistbyorderidRequest();
+                req1.GoodsWidth = 1L;
+                req1.GoodsHeight = 1L;
+                req1.GoodsWeight = "1.5";
+                req1.GoodsLength = 1L;
+                req1.OrderId = Convert.ToInt64(orderData.AliExpressOrderNumber);
+                AliexpressLogisticsRedefiningGetonlinelogisticsservicelistbyorderidResponse rsp1 = client.Execute(req1, SessionManager.GetAccessToken().access_token);
+                var LogisticsNo = string.Empty;
+                if (rsp1.ResultList.Count > 0)
+                {
+                    LogisticsNo = rsp1.ResultList[0].LogisticsServiceId;
+                }
+
+                //AliexpressSolutionOrderInfoGetRequest req2 = new AliexpressSolutionOrderInfoGetRequest();
+                //AliexpressSolutionOrderInfoGetRequest.OrderDetailQueryDomain obj1 = new AliexpressSolutionOrderInfoGetRequest.OrderDetailQueryDomain();
+                //obj1.ExtInfoBitFlag = 11111L;
+                //obj1.OrderId = Convert.ToInt64(orderData.AliExpressOrderNumber);
+                //req2.Param1_ = obj1;
+                //AliexpressSolutionOrderInfoGetResponse rsp2 = client.Execute(req2, SessionManager.GetAccessToken().access_token);
+
+                AliexpressSolutionOrderFulfillRequest req = new AliexpressSolutionOrderFulfillRequest();
+                req.ServiceName = "CAINIAO_G2G_DIRECTSHIP";
+                req.TrackingWebsite = orderData.OrignalProductLink;
+                req.OutRef = orderData.AliExpressOrderNumber;
+                req.SendType = "part";
+                req.Description = "memo";
+                req.LogisticsNo = LogisticsNo;
+                AliexpressSolutionOrderFulfillResponse rsp = client.Execute(req, SessionManager.GetAccessToken().access_token);
+                var data = rsp;
+            }
+            catch (Exception ex)
+            {
+                result = false;
+                logger.Info(ex.ToString());
+            }
+
+            return result;
         }
     }
 }
